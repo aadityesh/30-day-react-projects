ae0f92fc40f52661b2987cc2059300f367c820a3
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-06-30 21:26:58 +0530

    Day 2 Completed

--- day-2-random-colour/src/App.jsx
+++ day-2-random-colour/src/App.jsx
@@ -2,63 +2,51 @@ import { useState } from "react";
 
 // import "./App.css";
 
-function handleColours() {
-  const colors = [
-    "red",
-    "green",
-    "blue",
-    "purple",
-    "lavender",
-    "pink",
-    "yellow",
-    "peach",
-  ];
-  const min = 0;
-  const max = colors.length;
-  const random = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(random);
+function randomColour() {
+  const letters = "0123456789ABCDEF";
+  let colour = "#";
 
-  return colors[random];
+  for (let i = 0; i < 6; i++) {
+    colour += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return colour;
+}
+
+function handleClick(e) {
+  const body = document.querySelector("body");
+  body.style.background = randomColour();
+  e.target.style.backgroundColor = randomColour();
 }
 
 function App() {
-  const [count, setCount] = useState(0);
   const color = "red";
   return (
     <>
-      <div
-        className="flex flex-col justify-center items-center h-[100vh] w-[100vw]"
-        style={{ background: handleColours() }}
-      >
-        <h1 className="text-8xl" style={{ background: handleColours() }}>
-          Hello Colour
-        </h1>
+      <div className="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
+        <h1 className="text-8xl">Hello Colour</h1>
         <div className="flex justify-center gap-4 py-[150px]">
           <button
-            className="w-[200px] border border-black rounded "
-            style={{ background: handleColours() }}
-            onClick={handleColours}
+            className="w-[200px] border-[2px] border-black rounded "
+            onClick={(e) => handleClick(e)}
           >
             One
           </button>
           <button
-            className=" h-[50px] w-[200px] border border-black rounded "
-            style={{ background: handleColours() }}
-            onClick={handleColours}
+            className=" h-[50px] w-[200px] border-[2px] border-black rounded "
+            onClick={(e) => handleClick(e)}
           >
             Two
           </button>
           <button
-            className=" h-[50px] w-[200px] border border-black rounded "
-            style={{ background: handleColours() }}
-            onClick={handleColours}
+            className=" h-[50px] w-[200px] border-[2px] border-black rounded "
+            onClick={(e) => handleClick(e)}
           >
             Three
           </button>
           <button
-            className=" h-[50px] w-[200px] border border-black rounded "
-            style={{ background: handleColours() }}
-            onClick={handleColours}
+            className=" h-[50px] w-[200px] border-[2px] border-black rounded "
+            onClick={(e) => handleClick(e)}
           >
             Four
           </button>
